d80acb53aa543e385687bcbb5320b77962d70e94
Briand Alpa <briand.devs@gmail.com>
2026-01-28 21:48:22 +0700

    chore: initialize prisma and configure supabase connection

---
+++ prisma/migrations/20260128143802_add_users/migration.sql
@@ -0,0 +1,26 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('CUSTOMER', 'ORGANIZER');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "UserRole" NOT NULL DEFAULT 'CUSTOMER',
+    "avatar_url" TEXT,
+    "phone" TEXT,
+    "bio" TEXT,
+    "referral_code" TEXT NOT NULL,
+    "points" INTEGER NOT NULL DEFAULT 0,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_referral_code_key" ON "users"("referral_code");
